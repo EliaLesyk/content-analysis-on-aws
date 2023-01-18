9dfcf674ae95579ca0d92d349e03c105b89736db
Elina Lesyk <56804907+EliaLesyk@users.noreply.github.com>
2023-01-18 15:35:47 +0100

    Update Header

--- source/website/src/components/Header.vue
+++ source/website/src/components/Header.vue
@@ -6,7 +6,7 @@
       variant="dark"
     >
       <b-navbar-brand to="/">
-        AWS Content Analysis
+        AWS MediVision
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse" />
 
